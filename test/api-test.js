b0a5904f04a151e90763f3d68d1466ee5ee44bec
Eli White <github@eli-white.com>
2015-03-08 16:15:18 -0700

    Testing getBuild for non completed builds

--- test/api-test.js
+++ test/api-test.js
@@ -109,7 +109,91 @@ describe('Api', function() {
           assert.equal(body.status, 'success');
         });
       });
+    });
+  });
 
+  describe('getBuild', function() {
+    describe('should fail', function() {
+      beforeEach(function() {
+        instance = api.get('/api/getBuild');
+      });
+
+      describe('with invalid params', function() {
+        it('should error 400', function() {
+          return instance.expect(400);
+        });
+
+        it('should have message', function() {
+          return instance.expect(function(data) {
+            var body = data.body;
+
+            assert.equal(body.status, 'failure');
+            assert.equal(body.message, 'invalid arguments');
+          });
+        });
+      });
+
+      describe('with unknown build', function() {
+        beforeEach(function() {
+          instance = instance.send({
+            id: 'foo'
+          });
+        });
+
+        it('should error 400', function() {
+          return instance.expect(400);
+        });
+
+        it('should have message', function() {
+          return instance.expect(function(data) {
+            var body = data.body;
+
+            assert.equal(body.status, 'failure');
+            assert.equal(body.message, 'unknown build');
+          });
+        });
+      });
+    });
+
+    describe('with valid build', function() {
+      var buildOptions = {
+        head: 'asdf',
+        base: 'fdsa',
+        numBrowsers: 2
+      };
+      var buildId;
+
+      beforeEach(function() {
+        return api.post('/api/startBuild')
+        .send(buildOptions)
+        .then(function(res) {
+          var body = res.body;
+          buildId = body.build;
+        });
+      });
+
+      it('has pending build', function() {
+        return api.get('/api/getBuild')
+        .send({
+          id: buildId
+        })
+        .expect(200)
+        .expect(function(data) {
+          var body = data.body;
+          assert.equal(body.id, buildId);
+          assert.equal(body.head, buildOptions.head);
+          assert.equal(body.base, buildOptions.base);
+          assert.equal(body.status, 'pending');
+        });
+      });
+
+      xit('has successful build with no diffs', function() {
+
+      });
+
+      xit('has failed build with diffs', function() {
+
+      });
     });
   });
 });
